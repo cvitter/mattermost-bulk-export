577a1df9464e53320bd6ed9a244f7011ef35de4a
Craig Vitter <craig.vitter@gmail.com>
2018-07-06 20:59:03 -0400

    Just saving progress... nothing to see here

---
+++ export.py
@@ -0,0 +1,40 @@
+import json
+from datetime import datetime 
+#import database_functions
+
+def create_filename():
+    """
+    Create unique file name for each export file yyyy-mm-dd-hh-mm-ss.json
+    """
+    return datetime.now().strftime('%Y-%m-%d-%H-%M-%S') + ".json"
+
+"""
+export_object will hold each object (Version, Team, Channel, User, 
+Post, DirectChannel, and DirectPost) that we create in the export
+"""
+export_object = {}
+export_object[0] = {"type": "version", "version": 1}
+
+"""
+Team
+"""
+
+"""
+Channel
+"""
+
+"""
+User
+"""
+
+"""
+Post
+"""
+
+
+"""
+Open a file for writing, iterate over the export_object and write
+each item onto a single line in JSONL format
+"""
+for key, value in export_object.iteritems():
+    print value 
