919d48123538869ca7bb3aa1c50090dc746d0c8e
Craig Vitter <craig.vitter@gmail.com>
2018-07-07 12:10:42 -0400

    Test select all teams

--- database_functions.py
+++ database_functions.py
@@ -6,7 +6,29 @@ def get_teams(team_list):
     Takes
     """
     teams = {}
-    
+    team_row = 0;
+
+    if len(team_list) == 0:
+        sql_query = "SELECT " + \
+                    "DisplayName, Name, Type, Description, AllowOpenInvite " + \
+                    "FROM mattermost.Teams;"
+
+        db = connect(db_url, db_user, db_password, db_name)
+        cursor = db.cursor()
+        cursor.execute(sql_query)
+
+        for (display_name, name, type, description, allow_open_invite) in cursor:
+            teams[team_row] = {
+                "type" : "team",
+                "team" : {
+                    "name": name,
+                    "display_name": display_name,
+                    "type": type,
+                    "description": description,
+                    "allow_open_invite": allow_open_invite
+                }
+            }
+
     return teams
 
 
