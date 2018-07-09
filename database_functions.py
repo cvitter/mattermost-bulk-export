aac3691adc24d3d77ab383b2c6e7be8711e22f44
Craig Vitter <craig.vitter@gmail.com>
2018-07-09 10:24:36 -0400

    swap is for ==

--- database_functions.py
+++ database_functions.py
@@ -18,7 +18,7 @@ def get_channels(db_url, db_name, db_username, db_password, team_list,
                     "FROM mattermost.Channels" + \
                     "WHERE TeamId = '" + team_id + "'"
 
-        if export_deleted_channels is False:
+        if export_deleted_channels == False:
             sql_query += " AND DeleteAt = 0"
         sql_query += ";"
 
@@ -63,7 +63,7 @@ def get_team_ids(db_url, db_name, db_username, db_password, team_list,
                 where_clause += " OR "
             list_pos += 1
 
-    if export_deleted_teams is False:
+    if export_deleted_teams == False:
         """
         Only export teams that have a DeleteAt = 0
         """
@@ -109,7 +109,7 @@ def get_teams(db_url, db_name, db_username, db_password, team_list,
                 where_clause += " OR "
             list_pos += 1
 
-    if export_deleted_teams is False:
+    if export_deleted_teams == False:
         """
         Only export teams that have a DeleteAt = 0
         """
